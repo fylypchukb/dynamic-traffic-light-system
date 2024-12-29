2f61b3861344a9bca0128513160847d7e6746311
Bohdan Fylypchuk <fylypchukb@gmail.com>
2024-12-29 21:37:19 +0200

    add comments

--- server/DynamicTrafficLightServer/DynamicTrafficLightServer/Dtos/IntersectionDto/IntersectionRequestModel.cs
+++ server/DynamicTrafficLightServer/DynamicTrafficLightServer/Dtos/IntersectionDto/IntersectionRequestModel.cs
@@ -1,3 +1,22 @@
 ﻿namespace DynamicTrafficLightServer.Dtos;
 
-public record IntersectionRequestModel(string City, string Location, bool IsActive);
+/// <summary>
+/// Represents the data required to create or update an intersection.
+/// </summary>
+public record IntersectionRequestModel
+{
+    /// <summary>
+    /// The name of the city where the intersection is located.
+    /// </summary>
+    public required string City { get; set; }
+
+    /// <summary>
+    /// The specific location details of the intersection.
+    /// </summary>
+    public required string Location { get; set; }
+
+    /// <summary>
+    /// Indicates whether the intersection is active.
+    /// </summary>
+    public bool IsActive { get; set; }
+}
